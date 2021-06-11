6a012bfc4e171db83c2c79e9b69713f526c35cdd
muhammedrishadpe <rishadpe@gmail.com>
2021-06-12 02:06:39 +0530

    initial commit

---
+++ Js/app.js
@@ -0,0 +1,181 @@
+// resources in description
+const mainTabs = document.querySelector(".main-tabs");
+const mainSliderCircle = document.querySelector(".main-slider-circle");
+const roundButtons = document.querySelectorAll(".round-button");
+
+const colors = {
+  blue: {
+    50: {
+      value: "#e3f2fd"
+    },
+    100: {
+      value: "#bbdefb"
+    }
+  },
+  green: {
+    50: {
+      value: "#e8f5e9"
+    },
+    100: {
+      value: "#c8e6c9"
+    }
+  },
+  purple: {
+    50: {
+      value: "#f3e5f5"
+    },
+    100: {
+      value: "#e1bee7"
+    }
+  },
+  orange: {
+    50: {
+      value: "#ffe0b2"
+    },
+    100: {
+      value: "#ffe0b2"
+    }
+  },
+  red: {
+    50: {
+      value: "#ffebee"
+    },
+    100: {
+      value: "#ffcdd2"
+    }
+  }
+};
+
+const getColor = (color, variant) => {
+  return colors[color][variant].value;
+};
+
+const handleActiveTab = (tabs, event, className) => {
+  tabs.forEach((tab) => {
+    tab.classList.remove(className);
+  });
+
+  if (!event.target.classList.contains(className)) {
+    event.target.classList.add(className);
+  }
+};
+
+mainTabs.addEventListener("click", (event) => {
+  const root = document.documentElement;
+  const targetColor = event.target.dataset.color;
+  const targetTranslateValue = event.target.dataset.translateValue;
+
+  if (event.target.classList.contains("round-button")) {
+    mainSliderCircle.classList.remove("animate-jello");
+    void mainSliderCircle.offsetWidth;
+    mainSliderCircle.classList.add("animate-jello");
+
+    root.style.setProperty("--translate-main-slider", targetTranslateValue);
+    root.style.setProperty("--main-slider-color", getColor(targetColor, 50));
+    root.style.setProperty("--background-color", getColor(targetColor, 100));
+
+    handleActiveTab(roundButtons, event, "active");
+
+    if (!event.target.classList.contains("gallery")) {
+      root.style.setProperty("--filters-container-height", "0");
+      root.style.setProperty("--filters-wrapper-opacity", "0");
+    } else {
+      root.style.setProperty("--filters-container-height", "3.8rem");
+      root.style.setProperty("--filters-wrapper-opacity", "1");
+    }
+  }
+});
+
+
+// Cat BG
+
+function init() { 
+  const catWrapper = document.querySelector('.cat_wrapper')
+  const wrapper = document.querySelector('.wrapper')
+  const cat = document.querySelector('.cat')
+  const head = document.querySelector('.cat_head')
+  const legs = document.querySelectorAll('.leg')
+  const pos = {
+    x: null,
+    y: null
+  }
+
+  const walk = () =>{
+    cat.classList.remove('first_pose')
+    legs.forEach(leg=>leg.classList.add('walk'))
+  }
+
+  const handleMouseMotion = e =>{
+    pos.x = e.clientX
+    pos.y = e.clientY
+    walk()
+  }
+
+  const handleTouchMotion = e =>{
+    if (e.targetTouches) return
+    
+    pos.x = e.targetTouches[0].offsetX
+    pos.y = e.targetTouches[0].offsetY
+    walk()
+  }
+
+  const turnRight = () =>{
+    cat.style.left = `${pos.x - 90}px`
+    cat.classList.remove('face_left')
+    cat.classList.add('face_right')
+  }
+
+  const turnLeft = () =>{
+    cat.style.left = `${pos.x + 10}px`
+    cat.classList.remove('face_right')
+    cat.classList.add('face_left')
+  }
+
+  const decideTurnDirection = () =>{
+    cat.getBoundingClientRect().x < pos.x ?
+      turnRight()
+      :
+      turnLeft()
+  }
+
+  const headMotion = () =>{
+    pos.y > (wrapper.clientHeight - 100) ?
+      head.style.top = '-15px'
+      :
+      head.style.top = '-30px'
+  }
+
+  const jump = () =>{
+    catWrapper.classList.remove('jump')
+    if (pos.y < (wrapper.clientHeight - 250)) {
+      setTimeout(()=>{
+        catWrapper.classList.add('jump')
+      },100)
+    } 
+  }
+
+  const decideStop = ()=>{
+    if (cat.classList.contains('face_right') && pos.x - 90 === cat.offsetLeft ||
+        cat.classList.contains('face_left') && pos.x + 10 === cat.offsetLeft) {
+      legs.forEach(leg=>leg.classList.remove('walk'))    
+    }
+  }
+  
+  setInterval(()=>{
+    if (!pos.x || !pos.y) return
+    decideTurnDirection()
+    headMotion()
+    decideStop()
+  },100)
+
+  setInterval(()=>{
+    if (!pos.x || !pos.y) return
+    jump()
+  },1000)
+
+  document.addEventListener('mousemove', handleMouseMotion)
+  document.addEventListener('mousemove', handleTouchMotion)
+}
+
+window.addEventListener('DOMContentLoaded', init)
+
